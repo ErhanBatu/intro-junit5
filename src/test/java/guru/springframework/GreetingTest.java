9c796a277c416c45ed3ff5cc287e919668ea9408
Erhan <e.batu44@icloud.com>
2023-05-22 20:04:47 +0200

    40. Junit5 Test Instance Lifecyle Demo

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,15 +1,20 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     private Greeting greeting;
 
+    //this must be static, this is JUnit requirement
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before - I am only called Once!!!");
+    }
+
     @BeforeEach
     void setUp() {
-        System.out.println("In Before Each...");
+        System.out.println("In Before Each....");
         greeting = new Greeting();
     }
 
@@ -23,5 +28,20 @@ class GreetingTest {
     void helloWorld1() {
 
         System.out.println(greeting.helloWorld("John"));
+    }
+
+    @Test
+    void helloWorld2() {
+       System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each........");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("After!!! ***** - I am only called Once!!!");
     }
 }
